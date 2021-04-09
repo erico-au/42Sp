1aefb1ca1612be5f6f22824c634a2c1527b82907
eaugusto <eaugusto@student.42sp.org.br>
2021-04-09 15:52:11 +0000

    c01 ev01

--- exercices/C01/ex03/main.c
+++ exercices/C01/ex03/main.c
@@ -1,17 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_ft.c                                            :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: eaugusto <eaugusto@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/04/06 14:31:33 by eaugusto          #+#    #+#             */
-/*   Updated: 2021/04/08 15:32:56 by eaugusto         ###   ########.fr       */
+/*   Created: 2021/04/08 15:20:04 by eaugusto          #+#    #+#             */
+/*   Updated: 2021/04/08 17:01:15 by eaugusto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+void	ft_div_mod(int a, int b, int *div, int *mod);
 
-void	ft_ft(int *nbr)
+int		main(void)
 {
-	*nbr = 42;
+	int a;
+	int b;
+	int ret_div;
+	int ret_mod;
+
+	a = 6;
+	b = 4;
+	ft_div_mod(a, b, &ret_div, &ret_mod);
+	printf("%d\n", ret_div);
+	printf("%d\n", ret_mod);
+	return (0);
 }
--- exercices/C01/ex04/main.c
+++ exercices/C01/ex04/main.c
@@ -1,17 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_ft.c                                            :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: eaugusto <eaugusto@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/04/06 14:31:33 by eaugusto          #+#    #+#             */
-/*   Updated: 2021/04/08 15:32:56 by eaugusto         ###   ########.fr       */
+/*   Created: 2021/04/08 18:24:24 by eaugusto          #+#    #+#             */
+/*   Updated: 2021/04/08 18:33:21 by eaugusto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+void	ft_ultimate_div_mod(int *a, int *b);
 
-void	ft_ft(int *nbr)
+void main(void)
 {
-	*nbr = 42;
+	int z;
+	int w;
+	int *k;
+	int *j;
+
+	z = 5;
+	w = 2;
+	k = &z;
+	j = &w;
+	ft_ultimate_div_mod(k, j);
 }
