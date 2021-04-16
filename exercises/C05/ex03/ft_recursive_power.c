e2157c4b4f2a815136569c0c7a4cf93a8ced2539
eaugusto <eaugusto@student.42sp.org.br>
2021-04-16 17:30:42 +0000

    c05

--- exercises/C05/ex03/ft_recursive_power.c
+++ exercises/C05/ex03/ft_recursive_power.c
@@ -6,15 +6,15 @@
 /*   By: eaugusto <eaugusto@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/15 18:27:32 by eaugusto          #+#    #+#             */
-/*   Updated: 2021/04/15 18:44:19 by eaugusto         ###   ########.fr       */
+/*   Updated: 2021/04/16 16:44:10 by eaugusto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_iterative_power(int nb, int power);
+int	ft_recursive_power(int nb, int power);
 {
 	if (power < 0 )
 		return (0);
 	if (power > 0)
-		return nb * ft_iterative_power(nb, power - 1);
+		return nb * ft_recursive_power(nb, power - 1);
 	else return 1;
 }
--- exercises/C05/ex07/ft_find_next_prime.c
+++ exercises/C05/ex07/ft_find_next_prime.c
@@ -6,7 +6,7 @@
 /*   By: eaugusto <eaugusto@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/16 01:09:10 by eaugusto          #+#    #+#             */
-/*   Updated: 2021/04/16 01:23:37 by eaugusto         ###   ########.fr       */
+/*   Updated: 2021/04/16 16:50:19 by eaugusto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,4 +34,44 @@ int main()
 	int x = 98;
 
 	printf("numero = %d proximo = %d \n ", x, ft_find_next_prime(x));
+}
+
+
+int		ft_is_prime2(int nb)
+{
+	int a;
+	int test;
+
+	test = 0;
+	if (nb < 0)
+		return (0);
+	if (nb == 0 || nb == 1)
+		return (0);
+	else
+	{
+		a = nb;
+		while (a > 1)
+		{
+			if (nb % a == 0)
+				test++;
+			if (test > 1)
+				return (0);
+			a--;
+		}
+	}
+	return (1);
+}
+
+int		ft_find_next_prime(int nb)
+{
+	int	n;
+
+	n = nb;
+	if (nb >= 2147483630)
+		return (2147483647);
+	if (nb < 3)
+		return (2);
+	while (ft_is_prime2(n) != 1)
+		n++;
+	return (n);
 }
